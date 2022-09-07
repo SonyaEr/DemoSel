8bb9e28b52068d26cb743b5eff19e848b303e606
Sofia <yeroshenko.sofia@gmail.com>
2022-09-07 07:20:29 +0200

    tests

--- src/test/java/com/epam/TestAvic.java
+++ src/test/java/com/epam/TestAvic.java
@@ -3,10 +3,7 @@ package com.epam;
 import org.openqa.selenium.*;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.interactions.Actions;
-import org.openqa.selenium.support.ui.ExpectedConditions;
-import org.openqa.selenium.support.ui.FluentWait;
-import org.openqa.selenium.support.ui.Wait;
-import org.openqa.selenium.support.ui.WebDriverWait;
+import org.openqa.selenium.support.ui.*;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.BeforeTest;
@@ -30,6 +27,7 @@ public class TestAvic {
     public void profileSetUp() {
         System.setProperty("webdriver.chrome.driver", "src\\main\\resources\\chromedriver.exe");
     }
+
     @BeforeMethod
     public void setUp() {
         driver = new ChromeDriver();
@@ -45,23 +43,30 @@ public class TestAvic {
         WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(5));
         wait.until(ExpectedConditions.visibilityOfElementLocated(By.id("js_cart")));
         driver.findElements(By.className("btn-add")).get(1).click();
-        driver.manage().timeouts().implicitlyWait(Duration.ofMillis(500));
+        wait.until(ExpectedConditions.presenceOfElementLocated(xpath("(//div[@class='item'])[2]")));
         String tittle = driver.findElement(By.xpath("((//a[@class='btn-add'])[1]/following::a)[2]")).getText();
-        WebElement el = driver.findElement(xpath("//div[@class='item']//span[contains(text(),'"+ tittle +"')]"));
+        WebElement el = driver.findElement(xpath("//div[@class='item']//span[contains(text(),'" + tittle + "')]"));
         assertTrue(el.isDisplayed());
 
     }
 
     @Test(priority = 2)
-    public void checkAmountAfterAddQuantity()  {
+    public void checkAmountAfterAddQuantity() {
         driver.findElement(By.id("input_search")).sendKeys("samsung");
         driver.findElement(By.cssSelector("button.button-reset.search-btn")).click();
         driver.findElements(By.className("prod-cart__buy")).get(1).click();
-        WebDriverWait wait = new WebDriverWait(driver, Duration.ofMillis(500));
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofMillis(600));
         wait.until(ExpectedConditions.visibilityOfElementLocated(By.id("js_cart")));
-        WebDriverWait wait2 = new WebDriverWait(driver, Duration.ofMillis(800));
-        wait2.until(ExpectedConditions.elementToBeClickable(By.xpath("(//span[contains(@class,'plus')])[1]")));
+        String total_price_old = driver.findElement(xpath("//div[contains(@class,'item-total')]/span[contains(@class,'prise')]")).getText();
+        driver.findElements(By.className("btn-add")).get(1).click();
+        wait.until(ExpectedConditions.elementToBeClickable(xpath("(//span[contains(@class,'plus')])[1]")));
         driver.findElement(xpath("(//span[contains(@class,'plus')])[1]")).click();
+        WebElement m = (new WebDriverWait(driver, Duration.ofSeconds(8))).until((ExpectedCondition<WebElement>) webDriver -> {
+            if (!Objects.equals(total_price_old, driver.findElement(xpath("//div[contains(@class,'item-total')]/span[contains(@class,'prise')]")).getText())) {
+                return driver.findElement(xpath("//div[contains(@class,'item-total')]/span[contains(@class,'prise')]"));
+            }
+            return null;
+        });
         List<WebElement> prices = driver.findElements(xpath("//div[contains(@class,'total-h')]/span[contains(@class,'prise')]"));
         int number_new = 0;
         for (int i = 0; i < prices.size(); i++) {
@@ -71,20 +76,9 @@ public class TestAvic {
             }
             int total_h = Integer.parseInt(part[0]);
             int count = Integer.parseInt(driver.findElement(xpath("(//input[@class='js-changeAmount count'])[" + i + "+1]")).getAttribute("value"));
-            number_new = total_h * count;
+            number_new = number_new+total_h * count;
         }
-        String total_price_old = driver.findElement(xpath("//div[contains(@class,'item-total')]/span[contains(@class,'prise')]")).getText();
-
-        Wait<WebDriver> wait3 = new FluentWait<>(driver).withTimeout(Duration.ofSeconds(3)).pollingEvery(Duration.ofSeconds(1)).ignoring(NoSuchElementException.class);
-        Function<WebDriver, Boolean> function = i -> {
-           if(!Objects.equals(total_price_old, driver.findElement(xpath("//div[contains(@class,'item-total')]/span[contains(@class,'prise')]")).getText()))
-           {
-               return true;
-           }
-            return false;
-        };
-        wait3.until(function);
-        String total_price = driver.findElement(xpath("//div[contains(@class,'item-total')]/span[contains(@class,'prise')]")).getText();
+       String total_price = driver.findElement(xpath("//div[contains(@class,'item-total')]/span[contains(@class,'prise')]")).getText();
         String[] part2 = total_price.split(" ");
         if (part2.length != 2) {
             throw new IllegalArgumentException();
@@ -106,17 +100,17 @@ public class TestAvic {
     public void checkSliderMenuHrefValid() {
         Actions actions = new Actions(driver);
         ArrayList<String> ref = new ArrayList<>();
-         List<WebElement> menu = driver.findElements(By.xpath("//div[@class='menu-lvl first-level']/ul/li[@class='parent js_sidebar-item']"));
+        List<WebElement> menu = driver.findElements(By.xpath("//div[@class='menu-lvl first-level']/ul/li[@class='parent js_sidebar-item']"));
         for (int i = 0; i < menu.size(); i++) {
             actions.moveToElement(menu.get(i)).perform();
-            ref.add(driver.findElement(By.xpath("(//div[@class='menu-lvl first-level']/ul/li[@class='parent js_sidebar-item']/a)[" +i+ "+1]")).getAttribute("href"));
-              List<WebElement> sub_menu = driver.findElements(xpath("(//div[@class='menu-lvl second-level next-level js_next-level'])[" + i + "+1]/ul/li[@class='parent js_sidebar-item']"));
+            ref.add(driver.findElement(By.xpath("(//div[@class='menu-lvl first-level']/ul/li[@class='parent js_sidebar-item']/a)[" + i + "+1]")).getAttribute("href"));
+            List<WebElement> sub_menu = driver.findElements(xpath("(//div[@class='menu-lvl second-level next-level js_next-level'])[" + i + "+1]/ul/li[@class='parent js_sidebar-item']"));
             for (int j = 0; j < sub_menu.size(); j++) {
-                ref.add(driver.findElement(By.xpath("((//div[@class='menu-lvl second-level next-level js_next-level'])[" + i + "+1]/ul/li[@class='parent js_sidebar-item']/a)[" +j+ "+1]")).getAttribute("href"));
-                 List<WebElement> sub2_menu = driver.findElements(xpath("((//ul[@class='sidebar-list'])[" + i + "+1]/li[@class='parent js_sidebar-item'])[" + j + "+1]//li[@class='single-hover-block']/a"));
+                ref.add(driver.findElement(By.xpath("((//div[@class='menu-lvl second-level next-level js_next-level'])[" + i + "+1]/ul/li[@class='parent js_sidebar-item']/a)[" + j + "+1]")).getAttribute("href"));
+                List<WebElement> sub2_menu = driver.findElements(xpath("((//ul[@class='sidebar-list'])[" + i + "+1]/li[@class='parent js_sidebar-item'])[" + j + "+1]//li[@class='single-hover-block']/a"));
                 for (int k = 0; k < sub2_menu.size(); k++) {
-                      ref.add(sub2_menu.get(k).getAttribute("href"));
-                  }
+                    ref.add(sub2_menu.get(k).getAttribute("href"));
+                }
                 actions.moveToElement(sub_menu.get(j)).perform();
             }
         }
@@ -127,17 +121,17 @@ public class TestAvic {
 
         Iterator<String> it = set.iterator();
         ArrayList<String> invalid = new ArrayList<>();
-        while(it.hasNext()){
+        while (it.hasNext()) {
             url = it.next();
-            if(url == null || url.isEmpty()){
+            if (url == null || url.isEmpty()) {
                 continue;
             }
             try {
-                huc = (HttpURLConnection)(new URL(url).openConnection());
+                huc = (HttpURLConnection) (new URL(url).openConnection());
                 huc.setRequestMethod("HEAD");
                 huc.connect();
                 respCode = huc.getResponseCode();
-                if(respCode >= 400){
+                if (respCode >= 400) {
                     invalid.add(url);
                 }
             } catch (IOException e) {
